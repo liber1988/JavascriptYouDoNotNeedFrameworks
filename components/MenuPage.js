df9f6faf0eb461da331f7b641dc19702d5046263
liber1988 <92869703+liber1988@users.noreply.github.com>
2025-05-20 13:48:07 +0300

    Elements

--- components/MenuPage.js
+++ components/MenuPage.js
@@ -1,6 +1,14 @@
 export default class MenuPage extends HTMLElement {
   constructor() {
     super();
+    const template = document.getElementById("menu-page-template");
+    const content = template.content.cloneNode(true);
+    this.appendChild(content);
+  }
+  connectedCallback() {
+    const template = document.getElementById("menu-page-template");
+    const content = template.content.cloneNode(true);
+    this.appendChild(content);
   }
 }
-customElements.define("menu-page", DetailsPage);
+customElements.define("menu-page", MenuPage);
