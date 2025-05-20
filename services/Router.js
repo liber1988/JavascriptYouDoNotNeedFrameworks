8e4a48838f8f40c7c3f74638bd992de53c02f745
liber1988 <92869703+liber1988@users.noreply.github.com>
2025-05-20 12:00:30 +0300

    First commit

---
+++ services/Router.js
@@ -0,0 +1,48 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
+      a.addEventListener("click", (event) => {
+        event.preventDefault();
+        const url = event.target.getAttribute("href");
+        console.log(url);
+        Router.go(url);
+      });
+    });
+    window.addEventListener("popstate", (event) => {
+      Router.go(event.state.route, false);
+    });
+    Router.go(location.pathname);
+  },
+  go: (route, addToHistory = true) => {
+    if (addToHistory) {
+      history.pushState({ route }, "", route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "you Order";
+        break;
+      default:
+        if (route.startsWith("/product")) {
+          pageElement = document.createElement("h1");
+          pageElement.textContent = "Details";
+          const paramId = route.substring(route.lastIndexOf("-") + 1);
+          pageElement.dataset.id = paramId;
+        }
+    }
+    if (pageElement) {
+      const cache = document.querySelector("main");
+      cache.innerHTML = "";
+      cache.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
+  },
+};
+
+export default Router;
