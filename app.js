cbe1ac9252295cc826e23671f352433992337a90
liber1988 <92869703+liber1988@users.noreply.github.com>
2025-05-21 10:23:02 +0300

    full vanilla app

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ import Store from "./services/Store.js";
 import API from "./services/API.js";
 import { loadData } from "./services/Menu.js";
 import Router from "./services/Router.js";
-import MenuPage from "./components/MenuPage.js";
-import DetailsPage from "./components/DetailsPage.js";
-import OrderPage from "./components/OrderPage.js";
+import { MenuPage } from "./components/MenuPage.js";
+import { DetailsPage } from "./components/DetailsPage.js";
+import { OrderPage } from "./components/OrderPage.js";
+import ProductItem from "./components/ProductIrem.js";
+import CartItem from "./components/CartItem.js";
 window.app = {};
 app.store = Store;
 app.router = Router;
@@ -12,4 +14,10 @@ app.router = Router;
 window.addEventListener("DOMContentLoaded", async () => {
   loadData();
   app.router.init();
+});
+window.addEventListener("appcartchange", (event) => {
+  const badge = document.getElementById("badge");
+  const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
+  badge.textContent = qty;
+  badge.hidden = qty == 0;
 });
